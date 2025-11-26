fc64fb2331b7d695bd8090b37138f9e62e82b252
dmg06072 <45023330+dmg06072@users.noreply.github.com>
2025-11-26 15:38:25 +0900

    Add files via upload

---
+++ 09_TX/src/main/java/com/example/demo/Domain/Common/Service/TxTestService.java
@@ -0,0 +1,84 @@
+package com.example.demo.Domain.Common.Service;
+
+
+import com.example.demo.Domain.Common.Dto.MemoDto;
+import com.example.demo.Domain.Common.Entity.Memo;
+import com.example.demo.Domain.Common.Mapper.MemoMapper;
+import com.example.demo.Domain.Common.Repository.MemoRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+@Service
+@Slf4j
+public class TxTestService {
+    @Autowired
+    private MemoRepository memoRepository;
+
+    public void addMemo() throws Exception
+    {
+        log.info("TxTestService's addMemo...");
+        Memo memo = Memo.builder()
+                .id(null)
+                .text("tx")
+                .writer("a")
+                .createAt(LocalDateTime.now())
+                .build();
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        throw new SQLException();
+//        memoRepository.save(memo);
+    }
+    @Transactional(rollbackFor = SQLException.class,transactionManager = "jpaTransactionManager")
+    public void addMemoTx() throws Exception
+    {
+        log.info("TxTestService's addMemoTx...");
+        Memo memo = Memo.builder()
+                .id(null)
+                .text("tx")
+                .writer("a")
+                .createAt(LocalDateTime.now())
+                .build();
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        memoRepository.save(memo);
+        memo.setId(null);
+        throw new SQLException();
+//        memoRepository.save(memo);
+    }
+
+
+    @Autowired
+    MemoMapper memoMapper;
+
+    public void addMemoWithMybatis(MemoDto dto) throws Exception{
+        dto.setId(9994L);
+        memoMapper.insert(dto);
+        dto.setId(9995L);
+        memoMapper.insert(dto);
+        dto.setId(9996L);
+        memoMapper.insert(dto);
+        throw new SQLException("예외발생!");
+    }
+    @Transactional(rollbackFor = SQLException.class ,transactionManager = "dataSourceTransactionManager")
+    public void addMemoWithMybatisTx(MemoDto dto) throws Exception {
+        dto.setId(8994L);
+        memoMapper.insert(dto);
+        dto.setId(8995L);
+        memoMapper.insert(dto);
+        dto.setId(8996L);
+        memoMapper.insert(dto);
+        throw new SQLException("예외발생!");
+    }
+
+}
---
+++ 09_TX/src/test/java/com/example/demo/Domain/Common/Service/TxTestServiceTest.java
@@ -0,0 +1,32 @@
+package com.example.demo.Domain.Common.Service;
+
+import com.example.demo.Domain.Common.Dto.MemoDto;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class TxTestServiceTest {
+    @Autowired
+    private TxTestService txTestService;
+
+    @Test
+    public void t1() throws Exception{
+//        txTestService.addMemo();
+        txTestService.addMemoTx();
+    }
+
+
+    @Test
+    public void t2() throws Exception{
+        txTestService.addMemoWithMybatis(new MemoDto(9990L,"a","a",null));
+    }
+
+    @Test
+    public void t3() throws Exception{
+        txTestService.addMemoWithMybatisTx(new MemoDto(8990L,"a","a",null));
+    }
+
+}
